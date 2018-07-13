8e697a08ec93d45e8872c476e186fbb9a0e2f877
Tyler Flint <tylerflint@gmail.com>
2018-07-13 14:34:15 -0600

    Docker configuration and setup

---
+++ config/application.rb
@@ -0,0 +1,24 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+# add a special :assets groups that is only required when not in production
+# or when the PRECOMPILE env is set
+if !Rails.env.production? || ENV["PRECOMPILE"]
+  Bundler.require(:assets)
+end
+
+module DemoBlog
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+  end
+end
